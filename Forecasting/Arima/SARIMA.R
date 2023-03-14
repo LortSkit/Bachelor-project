d814ba105950a2e421245b4cce9b81f84b3e9c23
vidisha04 <99336979+vidisha04@users.noreply.github.com>
2023-03-14 12:48:14 +0100

    small changes to find production forecasts
    Analysed and plot in Production-SARIMA-V2-Analysis

--- Forecasting/Arima/SARIMA.R
+++ Forecasting/Arima/SARIMA.R
@@ -3,38 +3,55 @@ setwd("C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject")
 library(forecast)
 library(ggplot2)
 
-df <- read.csv('pf_filled.csv')
+df <- read.csv('cf_filled.csv')
 
-arr <- df$prod_k28
+arr <- df$cons_k28
 
 i = 0
-j = 500
+j = 3000
 mses <- c()
 method <- c()
+train <- c()
 test <- c()
 forecasted <- c()
 f_upper_bounds <- c()
 f_lower_bounds <- c()
 p_value <-c()
 
-#while (j<length(arr)-24) {
-while (j<548) {
-  train <- arr[i:j]
+while (j<(length(arr)-24)) {
+  train_ <- arr[i:j]
   test_ <- arr[j:(j+23)]
   
+  print(c(i,j))
+  print(c(j,(j+23)))
+  
+  train <- append(train, train_)
   test <- append(test, test_)
   
-  train.ts <- ts(train, frequency=24)
+  train.ts <- ts(train_, frequency=24)
 
   fit <- auto.arima(train.ts, seasonal=TRUE, trace=FALSE)
   fc <- forecast(fit, h=24, level=c(95))
   
-  dummy <- checkresiduals(fit, plot=F, lag=24)
-  p_value <- append(p_value, dummy$p.value)
-  mses <- append(mses, mean(test_^2-fc$mean^2))
+  forecasted <- append(forecasted, fc$mean)
+  f_upper_bounds <- append(f_upper_bounds, fc$upper)
+  f_lower_bounds <- append(f_lower_bounds, fc$lower) 
   
+  dummy <- checkresiduals(fit, plot=F, lag=24)
+  
+  p_value <- append(p_value, dummy$p.value)
+  mse <- mean((test_-fc$mean)^2)
+  mses <- append(mses, mse)
+  print(sqrt(mse))
   
   i = i+24
   j = j+24
 }
 
+write.csv(mses,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/mses.csv',row.names = FALSE)
+write.csv(train,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/train.csv',row.names = FALSE)
+write.csv(test,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/test.csv',row.names = FALSE)
+write.csv(forecasted,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/forecasted.csv',row.names = FALSE)
+write.csv(f_upper_bounds,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/f_upper_bounds.csv',row.names = FALSE)
+write.csv(f_lower_bounds,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/f_lower_bounds.csv',row.names = FALSE)
+write.csv(p_value,'C:/Users/vidis/OneDrive/Desktop/Spring23/BachelorProject/p_value.csv',row.names = FALSE)
